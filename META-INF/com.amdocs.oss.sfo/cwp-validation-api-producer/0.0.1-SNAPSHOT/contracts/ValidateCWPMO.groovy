580d3f81caf5f19f7f6bfed56f3391c8797dfe49
Pavan7287 <45281309+Pavan7287@users.noreply.github.com>
2018-12-17 13:04:09 +0530

    Add files via upload

--- META-INF/com.amdocs.oss.sfo/cwp-validation-api-producer/0.0.1-SNAPSHOT/contracts/ValidateCWPMO.groovy
+++ META-INF/com.amdocs.oss.sfo/cwp-validation-api-producer/0.0.1-SNAPSHOT/contracts/ValidateCWPMO.groovy
@@ -18,8 +18,8 @@ Contract.make{
         jsonPath('$.age',byRegex('[2-9][0-9]'))
         jsonPath('$.salary',byRegex('[1-9][0-9]{5}'))
         jsonPath('$.designation',byRegex('[A-Za-z]+'))
-        jsonPath('$.address[0].zipCode',byRegex('[0-9]{5}'))
-        jsonPath('$.address[1].zipCode',byRegex('[0-9]{5}'))
+        jsonPath('$.address.[*].zipCode',byRegex('[0-9]{5}'))
+
 
 
     }
